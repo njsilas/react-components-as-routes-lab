1a7a72e9f934ef8c7f4bf5292ff86274476cb3ae
Nicholas Silas <njsilas@gmail.com>
2021-03-08 19:08:56 -0500

    commit tree 0f0b3bfbba2f08f89437d24cf96da503c29ec983

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,10 +3,29 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
+    
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       <ul>
+         {movies.map(movie =>(
+           <div>
+              <ul>
+              <li>{movie.title}</li>
+             <li>{movie.metascore}</li> 
+              <li>{movie.time}</li>
+              <li>{movie.genres}</li>
+                </ul> 
+           </div>
+         ))}
+       </ul>
+       
     </div>
   );
 };
 
 export default Movies;
+// <h1>{movie.title}</h1>
+    //{movie.title}</br>
+    //{movie.metascore}</br>
+      //</br>
+      //ul{}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'red',
+  textDecoration: 'none',
+  color: 'white',
+}
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkred'
+      }}
+      >Home</NavLink>
+    <NavLink
+    to="/movies"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkred'
+      }}
+      >Movies</NavLink>
+      <NavLink
+      to="/directors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkred'
+      }}
+      >Directors</NavLink>
+      <NavLink
+      to="/actors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkred'
+      }}
+      >Actors</NavLink>
     </div>
   );
 };
